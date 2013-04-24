3355344c16bce823a6986a2e8a5f7148d35cb08a
Christophe Sicard <sicard.christophe@gmail.com>
2013-04-24 14:03:50 +0200

    Fix scenario path (another try)

--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -4,14 +4,11 @@ files = ['test/e2e/scenario.js'];
 
 autoWatch = false;
 singleRun = true;
-logLevel = LOG_INFO;
+logLevel = LOG_DEBUG;
 logColors = true;
 browsers = ['Chrome'];
 
 proxies = {
-  // angular.js, angular-resource.js, etc
-  '/angular': 'http://localhost:8000/build/angular',
-  '/': 'http://localhost:8000/build/docs/'
 };
 
 junitReporter = {
